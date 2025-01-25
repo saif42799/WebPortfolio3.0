6d9a35f1867bd6c1c92af4062619735c8f7af68a
Saif <sshaikh4@ggc.edu>
2025-01-24 20:41:26 -0500

    updated a few things on pc

--- my-web-app/src/components/Contact.jsx
+++ my-web-app/src/components/Contact.jsx
@@ -1,14 +1,80 @@
 
-function Contact(){
+function Contact() {
 
-    return(
+    return (
         <header>
-            <div className="contactConatiner" id="contact">
+            <div className="contactContainer" id="contact">
                 <h1 className="CONTACT">CONTACT</h1>
+
+                <section>
+                 
+                   
+
+
+                   
+                    <div className="formContainer">
+
+                    <form action="https://api.web3forms.com/submit" method="POST" target="_blank">
+                        <input type="hidden" name="access_key" value="7d657a0f-b2be-4590-bade-8cae8414fad6"></input>
+
+
+                        <h2 className="letsConnect">Let's Connect</h2>
+                        <div className="row100">
+                            <div className="col">
+                                <div className="inputBox">
+                                    <input type="text" name="first_name" required="required"></input>
+                                    <span className="text">First Name</span>
+                                    <span className="line"></span>
+                                </div>
+                            </div>
+
+                            <div className="col">
+                                <div className="inputBox">
+                                    <input type="text" name="last_name" required="required"></input>
+                                    <span className="text">Last Name</span>
+                                    <span className="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row100">
+                            <div className="col">
+                                <div className="inputBox">
+                                    <input type="text" name="email" required="required"></input>
+                                    <span className="text">Email</span>
+                                    <span className="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row100">
+                            <div className="col">
+                                <div className="inputBox textarea">
+                                    <textarea required="required" name="messgae"></textarea>
+                                    <span className="text">Your Message...</span>
+                                    <span className="line"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="row100">
+                            <div className="col">
+                                <input className="formButton" type="submit" value="Send"></input>
+                            </div>
+                        </div>
+                        
+                    </form>
+                    </div>
+                    
+                </section>
+
             </div>
+
+
             
+
         </header>
-    ); 
+    );
 }
 
 export default Contact
--- my-web-app/src/components/Projects.jsx
+++ my-web-app/src/components/Projects.jsx
@@ -1,14 +1,161 @@
 
-function Projects(){
+function Projects() {
 
-    return(
+    return (
         <header>
             <div className="projectConatiner" id="projects">
+
+               
+
+
                 <h1 className="PROJECTS">PROJECTS</h1>
-            </div>
+                <div class="projectSectionGrid">
+                    
+                <div class="project" id="gridBox0">
+                    <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                        <img className="gridCenterImg" src="/src/assets/images/Group46.png" alt=""></img>
+                    </a>
+                </div>
+
+
+                    <div class="project" id="gridBox1">
+
+                        <div class="projectCardShell">
+                            <div class="projectCard" >
+                                <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                                    <img src="/src/assets/images/testImg.jpg" alt=""></img>
+                                </a>
+                                <div class="projectContent">
+                                    <h2>Project 1</h2>
+                                    <div class="social">
+                                        <div class="item">
+                                            <div class="title">Like</div>
+                                            <div class="count">205</div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="project" id="gridBox2">
+
+                        <div class="projectCardShell">
+                            <div class="projectCard">
+                                <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                                    <img src="/src/assets/images/testImg.jpg" alt=""></img>
+                                </a>
+                                <div class="projectContent">
+                                    <h2>Project 2</h2>
+                                    <div class="social">
+                                        <div class="item">
+                                            <div class="title">Like</div>
+                                            <div class="count">205</div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="project" id="gridBox3">
+
+                        <div class="projectCardShell">
+                            <div class="projectCard">
+                                <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                                    <img src="/src/assets/images/testImg.jpg" alt=""></img>
+                                </a>
+                                <div class="projectContent">
+                                    <h2>Project 3</h2>
+                                    <div class="social">
+                                        <div class="item">
+                                            <div class="title">Like</div>
+                                            <div class="count">205</div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="project" id="gridBox4">
+
+                        <div class="projectCardShell">
+                            <div class="projectCard">
+                                <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                                    <img src="/src/assets/images/testImg.jpg" alt=""></img>
+                                </a>
+                                <div class="projectContent">
+                                    <h2>Project 4</h2>
+                                    <div class="social">
+                                        <div class="item">
+                                            <div class="title">Like</div>
+                                            <div class="count">205</div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    <div class="project" id="gridBox5">
+
+                        <div class="projectCardShell">
+                            <div class="projectCard">
+                                <a href="https://drive.google.com/file/d/18yBe35NGoetcDh6veYGvHAuB2mmblX4F/view?usp=sharing" target="_blank">
+                                    <img src="/src/assets/images/Group46.png" alt=""></img>
+                                </a>
+                                <div class="projectContent">
+                                    <h2>Project 5</h2>
+                                    <div class="social">
+                                        <div class="item">
+                                            <div class="title">Like</div>
+                                            <div class="count">205</div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                    {/* <div class="githubLink">
+                        <a href="https://github.com/saif42799" target="_blank">
+                            <img src="/src/assets/images/githubLogo.png"></img>
+                        </a>
+                    </div>
+                 */}
             
-        </header>
-    ); 
+
+        </div>
+        </div>
+            
+        </header >
+    );
 }
 
 export default Projects
